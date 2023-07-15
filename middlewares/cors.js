47fa6bac4dc73dd7c7b8fae86f77b722bf897485
Aleksei Kruglykhin <mj669@yandex.ru>
2023-07-15 15:41:13 +0300

    fix cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,13 +1,9 @@
-const { allowedCors } = require('../utils/utils');
-
 module.exports = (req, res, next) => {
-  const { origin } = req.headers;
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+
+  res.header('Access-Control-Allow-Origin', '*');
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
